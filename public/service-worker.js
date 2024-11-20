691dae3bcacb9db718ededfb717007cfa99365cc
marcusdevsurfer <marcus.sea13@gmail.com>
2024-11-19 21:10:13 -0600

    add: caching files in browser

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,25 +1,24 @@
-// self.addEventListener('install', event => {
-//   event.waitUntil(
-//     caches.open('pwa-cache').then(cache => {
-//       return cache.addAll([
-//         '/',
-//         '/css/styles.css',
-//         '/js/main.js',
-//         '/images/splash.png',
-//         '/manifest.json'
-//       ]);
-//     })
-//   );
-// });
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('pwa-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/css/styles.css',
+        '/js/main.js',
+        '/images/splash.png',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
 
-// self.addEventListener('fetch', event => {
-//   event.respondWith(
-//     caches.match(event.request).then(response => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
-
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
 self.addEventListener('push', event => {
     const data = event.data.json();
