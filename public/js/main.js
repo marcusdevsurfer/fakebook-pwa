302275b4ff81d1c595e113c6b186fc66c60c0113
marcusdevsurfer <marcus.sea13@gmail.com>
2024-11-19 20:30:46 -0600

    refact: add of buttons to turn on and turn off the notifys

--- public/js/main.js
+++ public/js/main.js
@@ -43,21 +43,82 @@ const fetchPosts = async () => {
         const response = await fetch("https://jsonplaceholder.typicode.com/posts", requestOptions)
         const data = await response.json();
         data.forEach(post => {
-            const postDiv = document.createElement('div');
-            const postTitle = document.createElement('h2');
-            const postBody = document.createElement('p');
-            postDiv.classList.add('post');
-            postTitle.innerHTML = post.title;
-            postBody.innerHTML = post.body;
-            postDiv.appendChild(postTitle);
-            postDiv.appendChild(postBody);
-            postsContainer.appendChild(postDiv);
+            fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`, requestOptions)
+                .then(response => response.json())
+                .then(user => {
+                    const postDiv = document.createElement('div');
+                    const postTitle = document.createElement('h2');
+                    const postBody = document.createElement('p');
+                    const postAuthor = document.createElement('p');
+                    postDiv.classList.add('post');
+                    postAuthor.innerHTML = `${user.name}:`;
+                    postTitle.innerHTML = post.title;
+                    postBody.innerHTML = post.body;
+                    postDiv.appendChild(postAuthor);
+                    postDiv.appendChild(postTitle);
+                    postDiv.appendChild(postBody);
+                    postsContainer.appendChild(postDiv);
+                });
         });
     }
     catch (error) {
         console.log('error', error);
     }
 };
+
+const subscribeToNotifications = () => {
+    const notifyBtn = document.getElementById('notify-btn');
+    notifyBtn.addEventListener('click', () => {
+        if (Notification.permission === 'granted') {
+            alert('Notificaciones ya están activadas');
+            showNotificationWithInterval();
+        } else if (Notification.permission !== 'denied') {
+            Notification.requestPermission().then(permission => {
+                if (permission === 'granted') {
+                    console.log('Permiso concedido');
+                    showNotificationWithInterval();
+                }
+            });
+        }
+    });
+}
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+}
+
+
+const revokeNotificationPermission = () => {
+    const revokeBtn = document.getElementById('revoke-notify-btn');
+    revokeBtn.addEventListener('click', () => {
+        if (Notification.permission === 'granted') {
+            Notification.permission = 'denied';
+            alert('Notificaciones apagadas');
+            alert('Se va a recargar la página');
+            window.location.reload();
+        } else {
+            console.warn('Notification permission is not granted');
+        }
+    });
+}
+
+
+const showNotificationWithInterval = () => {
+    setInterval(async () => {
+        if (Notification.permission !== 'granted') {
+            return;
+        }
+        try {
+            const randomUserId = getRandomInt(10) + 1;
+            const response = await fetch(`https://jsonplaceholder.typicode.com/users/${randomUserId}`);
+            const data = await response.json();
+            new Notification(`${data.name} ha publicado algo nuevo!`);
+        } catch (error) {
+            console.error('Error fetching user data for notification:', error);
+        }
+    }, 10000);
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const splashScreen = document.getElementById('splash-screen');
@@ -66,18 +127,8 @@ document.addEventListener('DOMContentLoaded', () => {
         splashScreen.style.display = 'none';
         homeScreen.style.display = 'block';
     }, 3000);
-    const notifyBtn = document.getElementById('notify-btn');
-    notifyBtn.addEventListener('click', () => {
-        if (Notification.permission === 'granted') {
-            new Notification('Hello from PWA!');
-        } else if (Notification.permission !== 'denied') {
-            Notification.requestPermission().then(permission => {
-                if (permission === 'granted') {
-                    new Notification('Hello from PWA!');
-                }
-            });
-        }
-    });
     fetchUsers();
     fetchPosts();
+    subscribeToNotifications()
+    revokeNotificationPermission();
 });
